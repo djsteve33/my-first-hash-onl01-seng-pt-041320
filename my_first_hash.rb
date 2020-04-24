0e4590a1928f130b259dc45053a5ccf66a28d995
Steve Womble <swomble@aol.com>
2020-04-24 15:55:41 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -12,14 +12,13 @@ def shipping_manifest
 end
 
 def retrieval
+  # Look up the value of the "oil paintings" key in the shipping_manifest hash below
   shipping_manifest = {
                   "whale bone corsets" => 5, 
                   "porcelain vases" => 2, 
                   "oil paintings" => 3
                  }
     shipping_manifest["oil paintings"]
-
-  # Look up the value of the "oil paintings" key in the shipping_manifest hash below
 end
 
 def adding
@@ -29,7 +28,7 @@ def adding
                   "oil paintings" => 3
                  }
   # add 2 muskets to the shipping_manifest hash below
-
+shipping_manifest["muskets"] = 2
 
 
   # add 4 gun powder to the shipping_manifest hash below
